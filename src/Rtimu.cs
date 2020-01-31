1d6a1be4150d1bea971aa45e84ef62d5b1557bdb
PepiteBrest <mathieu.piton@gmail.com>
2020-01-31 22:08:34 +0100

    Add sources file

---
+++ src/Rtimu.cs
@@ -0,0 +1,31 @@
+﻿using System.Management.Automation;
+
+namespace Pepitenet.Powershell.SenseHat
+{
+    [Cmdlet(VerbsCommon.Get, "Humidity")]
+    public class GetHumidity : Cmdlet
+    {
+        protected override void ProcessRecord()
+        {
+            Sense.RTIMU.RTIMUSettings Settings = Sense.RTIMU.RTIMUSettings.CreateDefault();
+            Sense.RTIMU.RTHumidity Humidity = Settings.CreateHumidity();
+            Sense.RTIMU.RTHumidityData Result = Humidity.Read();
+
+            WriteObject(Result);
+
+        }
+    }
+
+    [Cmdlet(VerbsCommon.Get, "Pressure")]
+    public class GetPressure : Cmdlet
+    {
+        protected override void ProcessRecord()
+        {
+            Sense.RTIMU.RTIMUSettings Settings = Sense.RTIMU.RTIMUSettings.CreateDefault();
+            Sense.RTIMU.RTPressure Pressure = Settings.CreatePressure();
+            Sense.RTIMU.RTPressureData Result = Pressure.Read();
+
+            WriteObject(Result);
+        }
+    }
+}
